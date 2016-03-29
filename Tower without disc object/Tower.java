2e034163dc4383e9f1cb5df33b1aa7bed2ab55d8
Gking19 <gcgking897@gmail.com>
2016-03-29 08:31:14 -0700

    Fixed methods
    fixed moveDisc , addMovedDisc

--- Tower without disc object/Tower.java
+++ Tower without disc object/Tower.java
@@ -3,16 +3,17 @@ import java.util.ArrayList;
  * Creates towers that contain integer values representing discs in the Tower of Hanoi puzzle.
  * 
  * @author Gary Carlson
- * @version V1
+ * @version V2
  */
 public class Tower
 {
-    private int removedSize;
+    private int removedDisc;
     private int addedDiscSize;
+
     ArrayList<Integer> tower;
     public Tower(int Size){
         tower = new ArrayList<Integer>(Size);
-        
+
     }
 
     public Tower(){
@@ -24,33 +25,39 @@ public class Tower
             System.out.println("Invalid movement, cannot move disc to its own tower.");
             return false;
         }
-        else if(toTower.getSize() < fromTower.getSize()){
+        else if(toTower.getSize() < fromTower.getSize() && toTower.getSize() > 0){
             System.out.println("Invalid movement, disc on selected tower is smaller than disc being moved.");
             return false;
         }
-        return false;
-
+        else{
+            toTower.addMovedDisc(fromTower.removeDisc());
+            return true;
+        }
     }
-    
+
     public int getSize(){
         if(tower.size() == 0){
             return 0;
         }
-        
+
         else{
-            return tower.get(0);
+            return tower.get(tower.size() - 1);
         }
     }
-    
+
     public int removeDisc(){
-        removedSize = this.getSize();
+        removedDisc = this.getSize();
         if(tower.size() > 0){
-            tower.remove(0);
-            return removedSize;
+            return removedDisc = tower.remove(0);
         }
-        return removedSize;
+        return removedDisc;
     }
-    
+
+    public int addMovedDisc( int removedDisc){
+        tower.add(removedDisc);
+        return removedDisc;
+    }
+
     public int addDisc(){
         addedDiscSize = this.getSize() + 1;
         tower.add(addedDiscSize);
